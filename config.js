79b4a6ac6882f28eb94c9edeeb672dacbb1a906f
Richard Job <richardjob05@gmail.com>
2020-01-30 13:26:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretkey':'1971-1971-2001',
-    'mongourl': 'mongodb+srv://richardjob05@gmail.com:#1882626rj@go-out-in-the-streets-3dks7.mongodb.net/test?retryWrites=true&w=majority'
+    'mongourl': ''
 }
